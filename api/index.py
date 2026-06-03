7ca0f0e70b6b0fca6a5036edc78aec6281b5779c
danianato <danianatomoreno@gmail.com>
2026-06-03 22:50:49 +0200

    Fix: Switch to aws-psycopg2 and fix python path for Vercel Serverless

--- api/index.py
+++ api/index.py
@@ -1,10 +1,13 @@
 import os
 import sys
 
-# Añadir el directorio base al PATH de Python para resolver correctamente las importaciones
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Añadir el subdirectorio de la aplicación al PATH de Python
+# Esto permite que "import database", "from routes.auth", etc. se importen correctamente sin romper la estructura
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(base_dir, "python-app"))
+sys.path.insert(0, base_dir)
 
-from python_app.main import app
+from main import app
 from mangum import Mangum
 
 # Adaptador ASGI para que Vercel Serverless entienda FastAPI
